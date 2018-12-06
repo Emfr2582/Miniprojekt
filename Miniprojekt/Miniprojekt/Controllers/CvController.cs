35b14b4512008091c17bdafeaef3bae37aa786fb
Emilia <emilia.franken@gmail.com>
2018-12-06 10:57:14 +0100

    new files with views added

--- Miniprojekt/Miniprojekt/Controllers/CvController.cs
+++ Miniprojekt/Miniprojekt/Controllers/CvController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,12 @@ namespace Miniprojekt.Controllers
 {
     public class CvController
     {
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
     }
 }
