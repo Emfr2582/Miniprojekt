62a5598e64107d77cb2c2c76081dcc9c7a72cadf
Emilia <emilia.franken@gmail.com>
2018-12-06 15:52:18 +0100

    added a finished form

---
+++ Miniprojekt/Miniprojekt/Models/CreateResumeService.cs
@@ -0,0 +1,20 @@
+﻿using Miniprojekt.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Miniprojekt.Models
+{
+    public class CreateResumeService
+    {
+        List<CreateResumeVM> people = new List<CreateResumeVM>();
+
+        public void AddPerson(CreateResumeVM person)
+        {
+            person.Id = people.Count == 0 ? 1 : people.Max(p => p.Id) + 1;
+
+            people.Add(person);
+        }
+    }
+}
