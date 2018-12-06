5bf06a638304324487c10df0e26d5c37eea6701e
Emilia <emilia.franken@gmail.com>
2018-12-06 13:58:53 +0100

    added View

---
+++ Miniprojekt/Miniprojekt/Controllers/CreateResumeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Miniprojekt.Controllers
+{
+    public class CreateResumeController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        [Route("")]
+        [Route("Create/Resume")]
+        public IActionResult CreateResume()
+        {
+            return Content("tja");
+        }
+    }
+}
