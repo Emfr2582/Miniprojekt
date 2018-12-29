c15855ca77bf8a0daaefe8e9828a050c33279f44
Emilia <emilia.franken@gmail.com>
2018-12-29 17:23:14 +0100

    failed to implement checkboxlist

--- Miniprojekt/Miniprojekt/Models/ViewModels/CreateResumeVM.cs
+++ Miniprojekt/Miniprojekt/Models/ViewModels/CreateResumeVM.cs
@@ -21,17 +21,28 @@ namespace Miniprojekt.Models.ViewModels
         public string Address { get; set; }
 
         [Required()]
-        [Display(Name ="Telefonnummer")]
+        [Display(Name = "Telefonnummer")]
         public string PhoneNumber { get; set; }
 
         [Required()]
-        [Display(Name ="E-mail")]
-        [EmailAddress(ErrorMessage ="Invalid e-mail adress")]
+        [Display(Name = "E-mail")]
+        [EmailAddress(ErrorMessage = "Invalid e-mail adress")]
         public string Email { get; set; }
 
-        [Display(Name ="Education")]
+        [Display(Name = "Education")]
         public SelectListItem[] EducationItems { get; set; }
 
         public string[] SelectedEducationValue { get; set; }
+
+        [Display(Name = "Gender")]
+        public SelectListItem[] Gender { get; set; }
+            public SelectListItem[] Gender = new SelectListItem[]
+            {
+                new SelectListItem { Value = "1", Text = "Kvinna"},
+                new SelectListItem {Value = "2", Text = "Man"},
+                new SelectListItem {Value = "3", Text = "Annat"},
+                new SelectListItem {Value = "4", Text = "Vill ej uppge"}
+            };
+        public int[] SelectedGenderValues{ get; set; }
     }
 }
